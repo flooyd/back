0a577a7dd0c30c53ff7451223645e59ecb4a4f61
Floyd <floydtjones@gmail.com>
2024-04-18 14:51:34 -0400

    initial

---
+++ src/user/user.controller.ts
@@ -0,0 +1,40 @@
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { UserService } from './user.service';
+import { CreateUserDto } from './dto/createUser.dto';
+import { UserResponseInterface } from './types/response';
+import { LoginUserDto } from './dto/loginUser.dto';
+
+@Controller()
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post('users')
+  @UsePipes(new ValidationPipe())
+  async createUser(
+    @Body('user') createUserDto: CreateUserDto,
+  ): Promise<UserResponseInterface> {
+    const user = await this.userService.createUser(createUserDto);
+    return this.userService.buildUserResponse(user);
+  }
+
+  @Get('users')
+  async findAll(): Promise<UserResponseInterface[]> {
+    const users = await this.userService.findAll();
+    return users.map((user) => this.userService.buildUserResponse(user));
+  }
+
+  @Post('users/login')
+  async login(
+    @Body('user') loginUserDto: LoginUserDto,
+  ): Promise<UserResponseInterface> {
+    const user = await this.userService.login(loginUserDto);
+    return this.userService.buildUserResponse(user);
+  }
+}
